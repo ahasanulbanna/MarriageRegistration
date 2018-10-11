973ddc328dcd154f3803cc34b420ff8773a2ab6d
Ahasanul Banna <ahasanulbanna@outlook.com>
2018-10-11 15:40:42 +0600

    Done So Far NidController -------Add NidServices #CreateNid

---
+++ MarriageRegistrar.Services/Services_NID/NidServices.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using MarriageRegistrar.Common;
+using MarriageRegitrar.Models;
+using MarriageRegitrar.Models.Models_NID;
+
+namespace MarriageRegistrar.Services.Services_NID
+{
+    
+    public class NidServices : INidServices
+    {
+        private readonly MarriageRegistrarDbContext _context;
+
+        public NidServices()
+        {
+            _context=new MarriageRegistrarDbContext();
+        }
+        public JsonResult CreateNid(NID nid)
+        {
+            try
+            {
+                var today = DateTime.Today;
+                var a = (today.Year * 100 + today.Month) * 100 + today.Day;
+                var b = (nid.DoB.Year * 100 + nid.DoB.Month) * 100 + nid.DoB.Day;
+                var age = (a - b) / 10000;
+                if (age >= 18)
+                {
+                    _context.Nids.Add(nid);
+                    _context.SaveChanges();
+                    Generator.IsReport = "Ok";
+                    Generator.Message = "Data Successfully Add on NID";
+                }
+                else
+                {
+                    Generator.IsReport = "NotOk";
+                    Generator.Message = "Data Not Add on NID";
+                }
+            }
+            catch (Exception ex)
+            {
+                Generator.IsReport = "NotOk";
+                Generator.Message = ex.Message;
+            }
+            return new JsonResult
+            {
+                Data = new
+                {
+                    Generator.IsReport,
+                    Generator.Message
+                },
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+    }
+
+
+    public interface INidServices
+    {
+        JsonResult CreateNid(NID nid);
+    }
+}
---
+++ MarriageRegitrar.API/Controllers/NidController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MarriageRegistrar.Services.Services_NID;
+using MarriageRegitrar.Models.Models_NID;
+
+namespace MarriageRegitrar.API.Controllers
+{
+    [RoutePrefix("Api/Nid")]
+    public class NidController : ApiController
+    {
+        private readonly INidServices _services;
+
+        public NidController()
+        {
+                _services=new NidServices();
+        }
+        [Route("CreateNid")]
+        [HttpPost]
+        public IHttpActionResult CreateNid(NID nid)
+        {
+            return Ok(_services.CreateNid(nid).Data);
+        }
+    }
+}
